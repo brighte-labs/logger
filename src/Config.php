5321c81eee0bad764c8065c3f0a2f36cb1f655c5
Fahad <mohammed.fahad@brighte.com.au>
2020-07-06 13:47:43 +1000

    Adding json formatter for elastic search.

--- src/Config.php
+++ src/Config.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace BrighteCapital\Logger;
 
@@ -118,4 +118,4 @@ class Config
     {
         return $this->sentry;
     }
-}//end class
+}
